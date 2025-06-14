e0e5c5d5980ee5b438e06a444d0580ac2b19c937
Danilo <danilo.piras-cons@nord-com.it>
2025-06-14 19:07:11 +0200

    Initial commit

---
+++ src/main/java/com/danilopiras/usermanagement/controller/UserManagementController.java
@@ -0,0 +1,15 @@
+package com.danilopiras.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/user")
+@Slf4j
+public class UserManagementController {
+	
+	
+
+}
